c75047acdc9d84156e7f4902d97f2df7d82e93ce
LucyMoore <lucyviolet.moore@gmail.com>
2016-06-16 19:39:24 +1200

    minimum functionality for host

--- components/fruit.js
+++ components/fruit.js
@@ -11,13 +11,13 @@ module.exports = class recentImgs extends React.Component {
 
 	render (){
 	    return (
-	    	<div id="fruit-pane" onClick={this.props.onClick}>>
+	    	<div id="fruit-pane" onClick={this.props.onClick}>
 					<table>
 					<tbody>
 						<tr>
 							<td></td>
 							<td></td>
-							<td id="a" onClick={this.props.onClick}></td>
+							<td id="a"></td>
 							<td id="b"></td>
 							<td id="c"></td>
 							<td id="d"></td>
@@ -26,22 +26,43 @@ module.exports = class recentImgs extends React.Component {
 						</tr>
 						<tr>
 							<td></td>
-							<td></td>
 							<td id="f" ></td>
 							<td id="g"></td>
 							<td id="h"></td>
 							<td id="i"></td>
 							<td id="j"></td>
-							<td></td>
+							<td id="k"></td>
+							<td id="l"></td>
+
+						</tr>
+						<tr>
+							<td ></td>
+							<td id="m"></td>
+							<td id="n"></td>
+							<td id="o"></td>
+							<td id="p"></td>
+							<td id="q"></td>
+							<td id="r"></td>
+							<td id="s"></td>
+						</tr>
+						<tr>
+							<td ></td>
+							<td id="t" ></td>
+							<td id="u"></td>
+							<td id="v"></td>
+							<td id="w"></td>
+							<td id="x"></td>
+							<td id="y"></td>
+							<td id="z"></td>
 						</tr>
 						<tr>
 							<td></td>
 							<td></td>
-							<td id="k" ></td>
-							<td id="l"></td>
-							<td id="m"></td>
-							<td id="n"></td>
-							<td id="o"></td>
+							<td id="aa"></td>
+							<td id="bb"></td>
+							<td id="cc"></td>
+							<td id="dd"></td>
+							<td id="ee"></td>
 							<td></td>
 						</tr>
 						</tbody>
--- index.js
+++ index.js
@@ -4,27 +4,63 @@ import ReactDOM from 'react-dom'
 import Fruit from './components/fruit'
 
 var counter = 0
+var levelup = 0
+document.getElementById('score').innerHTML= "Score: " + counter
+
+function go(){
+  level()
+  setInterval(level, 8000)
+}
+
+function level(){
+  levelup++
+  console.log(levelup)
+  if(levelup === 1){
+    setInterval(apple, 2000)
+  }
+  if(levelup === 2){
+    setInterval(apple, 1500)
+  }
+  if(levelup === 3){
+    setInterval(apple, 1000)
+  }
+  if(levelup === 4){
+    setInterval(apple, 500)
+  }
+  if(levelup === 5){
+    setInterval(apple, 400)
+  }
+  if(levelup === 6){
+    setInterval(apple, 300)
+  }
+  if(levelup >= 7){
+    alert("GAME OVER, your score " + counter)
+    location.reload();
+  }
+}
 
 function apple(){
   var d = new Date()
-  var numArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o']
-  var rand = Math.floor(Math.random()*14)+1
+  var numArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','aa','bb','cc','dd','ee']
+  var rand = Math.floor(Math.random()*30)+1
   console.log(numArr[rand])
   document.getElementById(numArr[rand]).style.backgroundImage="url('img/apple.jpg')"
 }
 
+
 function pick(e){
-  counter++
-  console.log(e.target.id)
-  document.getElementById(e.target.id).style.backgroundImage="url('')"
+  if(document.getElementById(e.target.id).style.backgroundImage){
+    counter++
+    console.log(e.target.id)
+    document.getElementById(e.target.id).style.backgroundImage="url('')"
+    document.getElementById('score').innerHTML = "Score: " + counter
+  }
 }
 
 ReactDOM.render(
-
-<div>
-<Fruit onClick={pick}/>
-<button onClick={setInterval(apple, 2000)}>Go</button>
-</div>,
-
- document.getElementById('content')
-)
+  <div>
+    <button onClick={go}>Go</button>
+    <Fruit onClick={pick}/>
+  </div>,
+   document.getElementById('content')
+  )
